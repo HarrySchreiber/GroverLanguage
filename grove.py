809d8af1262403c427f3c6d0e689658c24de1e2c
HarrySchreiber <hvs4th@gmail.com>
2021-05-10 19:12:38 -0400

    Added answer to the question posed

--- grove.py
+++ grove.py
@@ -1,8 +1,10 @@
-# TODO: Answer This Question
-# Is your Grove interpreter using a static or dynamic type system? Briefly explain what aspects of the interpreter make it so.
+# Is your Grove interpreter using a static or dynamic type system? Briefly explain what aspects of 
+# the interpreter make it so.
 # 
-# 
-# 
+# The Grove interpreter is dynamically typed. When defining variables or just evaluating types
+# the interpreter already knows what the type the value is of and we can change its type dynamically.
+# We also do not need to tell the interpreter what type a variable is before we assign it. For these 
+# reasons we know that our interpreter is dynamically typed.
 
 from grove_parse import *
 from grove_lang import *
